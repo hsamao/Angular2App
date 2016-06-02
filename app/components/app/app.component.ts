ebf6dbea98385c77264123549c28ace425f68e69
samao <hsamao86@gmail.com>
2016-06-01 22:08:13 -0400

    Added Pipe

--- app/components/app/app.component.ts
+++ app/components/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component, Inject, provide} from '@angular/core';
+import {Component, Inject, provide, Pipe, PipeTransform} from '@angular/core';
 
 import {MyComponent} from "../my-component/my-component.component.ts";
 import {TodosComponent} from "../todos/todos.component";
@@ -12,13 +12,37 @@ import {Engine} from "../../services/car/engine.service";
   selector: 'my-app',
   template: `
   <h1>Color: {{color}}</h1>
+  <h2>{{name | uppercase}}</h2>
+  <h2>{{name | lowercase}}</h2>
+  <h2>{{name | replace: 'Doe': 'Smith'}}</h2>
+
+  <h2>Today is {{today | date: 'longDate'}} and it is {{today | date: 'shortTime'}}</h2>
+  <h2>{{name}}'s birthday is {{birthday | date:'longDate'}}</h2>
+  <h2>{{number | currency:'USD': 'true'}}</h2>
+
+  <h2>{{promise | async}}</h2>
   `,
   providers: [Car, Body, Tires, Engine]
 })
 export class AppComponent {
   color: string;
+  name: string;
+  today: Date;
+  birthday: Date;
+  number: number;
+  promise;
 
   constructor(@Inject(Car) car) {
     this.color = car.body.color;
+    this.name = 'John Doe';
+    this.today = new Date();
+    this.birthday = new Date(1981, 09, 10);
+    this.number = 5;
+
+    this.promise = new Promise(function(resolve, reject){
+      setTimeout(function(){
+        resolve('Hey, I am a promise');
+      }, 2000);
+    });
   }
 }
